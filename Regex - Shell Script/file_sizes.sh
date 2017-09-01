a9c64c348edfcae133f5e91faea3bb5a3f431aa0
Mingkai Ma <mamingkai824@gmail.com>
2017-09-01 12:40:26 +1000

    Update file_sizes.sh

--- Regex - Shell Script/file_sizes.sh
+++ Regex - Shell Script/file_sizes.sh
@@ -1,3 +1,13 @@
+#Write a shell script file_sizes.sh which prints the names of the files in 
+#the current directory splitting them into three categories: small, medium-sized and large. 
+#A file is considered small if it contains less than 10 lines, medium-sized if contains less than 100 lines, 
+#otherwise it is considered large.
+#Your script should always print exactly three lines of output. 
+#Files should be listed in alphabetic order on each line. 
+#Your shell-script should match character-for-character the output shown in the example below. 
+#Notice the creation of a separate direcory for testing and the use of the script from the last question to produce test files. 
+#You could also produce test files manually using an editor.
+
 #!/bin/sh
 
 small="Small files:"
