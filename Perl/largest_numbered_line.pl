1b080e3e3003f4b5eb282d4a95b5f6255371ad56
Mingkai Ma <mamingkai824@gmail.com>
2017-09-30 23:13:47 +1000

    Add files via upload

---
+++ Perl/largest_numbered_line.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+@lines = <STDIN>;
+$max_value = "";
+
+foreach $line(@lines){
+    if($line !~ m/[0-9]/){
+        next;
+    }
+
+    my $temp_line = $line;
+    $temp_line =~ s/[^0-9\.\-]/ /g;
+    $temp_line =~ s/\s+/ /g;
+    $temp_line =~ s/\-+/\-/g;
+    $temp_line =~ s/\s*$//g;
+    $temp_line =~ s/\-+$//g;
+    $temp_line =~ s/^ //;
+    my @nb_list = split / /, $temp_line;
+    foreach(@nb_list){
+        if($_ !~ m/[0-9]/){
+            next;
+        }
+        if($max_value eq ""){
+            $max_value = $_;
+        }else{
+            if($max_value < $_){
+                $max_value = $_;
+            }
+        }
+    }
+}
+if($max_value eq ""){
+    exit;
+}
+
+foreach $line(@lines){
+    if($line =~ m/$max_value/){
+        print "$line";
+    }
+}
---
+++ Perl/middle_lines.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+open F, '<', $ARGV[0] or die;
+@lines = <F>;
+close F;
+$total = @lines;
+
+if($total == 0){
+    exit;
+}
+
+
+if($total % 2 == 0){
+    print "$lines[int($total/2) - 1]";
+    print "$lines[int($total/2)]";
+}else{
+    print "$lines[int($total/2)]";
+}
